76222a98796425ea983a5bd489ead579550e18fd
ohyecloudy <ohyecloudy@gmail.com>
2022-11-03 21:33:30 +0900

    :wrench: elixir 개발 환경 설정
    - dialyzer 설치
    - erlang 버전을 올림
    - db setting을 제거해 script 실행 가능 상태로 만듦
    - changelog는 안 쓴다

--- mix.exs
+++ mix.exs
@@ -7,22 +7,22 @@ defmodule Tbot800.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [ignore_warnings: ".dialyzer_ignore.exs"]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :eex]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
     ]
   end
 end
