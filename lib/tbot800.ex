2ea401506fc3d0b01bea6dba958eb9292deb63f9
ohyecloudy <ohyecloudy@gmail.com>
2022-11-03 21:25:07 +0900

    :tada: mix new

---
+++ lib/tbot800.ex
@@ -0,0 +1,18 @@
+defmodule Tbot800 do
+  @moduledoc """
+  Documentation for `Tbot800`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tbot800.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tbot800_test.exs
@@ -0,0 +1,8 @@
+defmodule Tbot800Test do
+  use ExUnit.Case
+  doctest Tbot800
+
+  test "greets the world" do
+    assert Tbot800.hello() == :world
+  end
+end
