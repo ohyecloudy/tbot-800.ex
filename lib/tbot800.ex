1af18bfc83c3d4648a1fb1a00c6b41a6e99e919a
ohyecloudy <ohyecloudy@gmail.com>
2023-01-26 21:39:45 +0900

    :star: mix run을 사용해서 html, tweet items를 생성
    - 배포할 것도 아니라서 escript는 번거롭다
    - mix run -e "Tbot800.run" -- [argv..]
      - 이런 식으로 실행하게 한다
      - 예) mix run -e "Tbot800.run" -- priv/source_list.exs ohyecloudy http://ohrepos.github.io/pquotes-repo/quotes/ priv/tweet_items.exs quotes

--- lib/tbot800.ex
+++ lib/tbot800.ex
@@ -1,18 +1,42 @@
 defmodule Tbot800 do
-  @moduledoc """
-  Documentation for `Tbot800`.
-  """
+  require Logger
+  alias Tbot800.Builder
 
-  @doc """
-  Hello world.
+  def run() do
+    apply(__MODULE__, :build, System.argv())
+  end
 
-  ## Examples
+  def build(source_path, twitter_account, base_url, tweet_items_path, html_root_dir) do
+    Logger.info("build tweet items & html pages")
 
-      iex> Tbot800.hello()
-      :world
+    Logger.info("=> read quote source file #{source_path}")
+    {source_list, _} = Code.eval_file(source_path)
 
-  """
-  def hello do
-    :world
+    Logger.info(
+      "=> make tweet items & html pages account: #{twitter_account}, base_url: #{base_url}"
+    )
+
+    %{tweet_items: tweet_items, webpages: webpages} =
+      Builder.build(source_list, twitter_account, base_url)
+
+    Logger.info("=> write tweet items: #{tweet_items_path}")
+
+    tweet_items
+    |> Enum.to_list()
+    |> inspect(limit: :infinity)
+    |> Code.format_string!()
+    |> then(&File.write!(tweet_items_path, &1, [:binary, :write]))
+
+    Logger.info("=> write html pages under root dir: #{html_root_dir}")
+    Logger.info("=> => mkdir: #{html_root_dir}")
+    File.mkdir_p!(html_root_dir)
+
+    for %{html: html, html_filename: filename} <- webpages do
+      path = Path.join(html_root_dir, filename)
+      Logger.info("=> => write html: #{path}")
+      File.write!(path, html, [:binary, :write])
+    end
+
+    Logger.info("Done")
   end
 end
--- test/tbot800_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Tbot800Test do
-  use ExUnit.Case
-  doctest Tbot800
-
-  test "greets the world" do
-    assert Tbot800.hello() == :world
-  end
-end
