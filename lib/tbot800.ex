2470ed7aca1f83c3b83e8e6b52ce940e9d28e6d6
ohyecloudy <ohyecloudy@gmail.com>
2023-01-27 22:17:53 +0900

    :star: OptionParser 모듈로 인자 파싱
    mix run -e "Tbot800.run" -- \
    --source-path priv/source_list.exs\
    --twitter-account ohyecloudy\
    --html-host-base-url http://ohrepos.github.io/pquotes-repo/quotes/\
    --output-tweet-items-path priv/tweet_items.exs\
    --output-html-root-dir quotes
    
    이런식으로 실행

--- lib/tbot800.ex
+++ lib/tbot800.ex
@@ -3,36 +3,59 @@ defmodule Tbot800 do
   alias Tbot800.Builder
 
   def run() do
-    apply(__MODULE__, :build, System.argv())
+    {args, []} =
+      OptionParser.parse!(System.argv(),
+        strict: [
+          source_path: :string,
+          twitter_account: :string,
+          html_host_base_url: :string,
+          output_tweet_items_path: :string,
+          output_html_root_dir: :string
+        ]
+      )
+
+    build(
+      args[:source_path],
+      args[:twitter_account],
+      args[:html_host_base_url],
+      args[:output_tweet_items_path],
+      args[:output_html_root_dir]
+    )
   end
 
-  def build(source_path, twitter_account, base_url, tweet_items_path, html_root_dir) do
+  def build(
+        source_path,
+        twitter_account,
+        html_host_base_url,
+        output_tweet_items_path,
+        output_html_root_dir
+      ) do
     Logger.info("build tweet items & html pages")
 
     Logger.info("=> read quote source file #{source_path}")
     {source_list, _} = Code.eval_file(source_path)
 
     Logger.info(
-      "=> make tweet items & html pages account: #{twitter_account}, base_url: #{base_url}"
+      "=> make tweet items & html pages account: #{twitter_account}, html_host_base_url: #{html_host_base_url}"
     )
 
     %{tweet_items: tweet_items, webpages: webpages} =
-      Builder.build(source_list, twitter_account, base_url)
+      Builder.build(source_list, twitter_account, html_host_base_url)
 
-    Logger.info("=> write tweet items: #{tweet_items_path}")
+    Logger.info("=> write tweet items: #{output_tweet_items_path}")
 
     tweet_items
     |> Enum.to_list()
     |> inspect(limit: :infinity)
     |> Code.format_string!()
-    |> then(&File.write!(tweet_items_path, &1, [:binary, :write]))
+    |> then(&File.write!(output_tweet_items_path, &1, [:binary, :write]))
 
-    Logger.info("=> write html pages under root dir: #{html_root_dir}")
-    Logger.info("=> => mkdir: #{html_root_dir}")
-    File.mkdir_p!(html_root_dir)
+    Logger.info("=> write html pages under root dir: #{output_html_root_dir}")
+    Logger.info("=> => mkdir: #{output_html_root_dir}")
+    File.mkdir_p!(output_html_root_dir)
 
     for %{html: html, html_filename: filename} <- webpages do
-      path = Path.join(html_root_dir, filename)
+      path = Path.join(output_html_root_dir, filename)
       Logger.info("=> => write html: #{path}")
       File.write!(path, html, [:binary, :write])
     end
