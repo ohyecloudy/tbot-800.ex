13f5bad496f9e3a0892579a048fe45973b412ff7
ohyecloudy <ohyecloudy@gmail.com>
2023-02-21 22:05:30 +0900

    :star: twitter sdk인 ExTwitter 패키지 추가

---
+++ lib/tbot800/twitter/oauth.ex
@@ -0,0 +1,19 @@
+defmodule Tbot800.Twitter.OAuth do
+  @type t :: %__MODULE__{
+          consumer_key: String.t(),
+          consumer_secret: String.t(),
+          access_token: String.t(),
+          access_token_secret: String.t()
+        }
+  defstruct consumer_key: "", consumer_secret: "", access_token: "", access_token_secret: ""
+
+  @spec new(String.t(), String.t(), String.t(), String.t()) :: t
+  def new(consumer_key, consumer_secret, access_token, access_token_secret) do
+    %__MODULE__{
+      consumer_key: consumer_key,
+      consumer_secret: consumer_secret,
+      access_token: access_token,
+      access_token_secret: access_token_secret
+    }
+  end
+end
--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Tbot800.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+      {:extwitter, "~> 0.14.0"}
     ]
   end
 end
